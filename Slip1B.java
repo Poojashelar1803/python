7438887c81b3ff36217133e685d4d1a674541f3d
Poojashelar1803 <poojashelar8767@gmail.com>
2025-07-31 19:51:29 +0530

    Add files via upload

---
+++ Slip1B.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+class Slip1B{
+
+    public static void main(String args[]) throws IOException{
+
+        char ch;
+
+        FileReader fr = new FileReader("a.txt");
+
+        FileWriter fw = new FileWriter("b.txt");
+
+        int c;
+
+        while ((c=fr.read())!=-1){
+
+            ch=(char)c;
+
+          if(Character.isDigit(ch)==false){
+
+              fw.write(c);
+
+          }
+
+        }
+
+        fr.close();
+
+        fw.close();
+
+    }
+
+
+}
