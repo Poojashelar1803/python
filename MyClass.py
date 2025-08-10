b4ad7de100819dbc363e7961387b50fb33cb915f
Poojashelar1803 <poojashelar8767@gmail.com>
2025-08-10 11:16:49 +0530

    Add files via upload

--- MyClass.py
+++ MyClass.py
@@ -1,10 +1,20 @@
-class MyClass:
- def get_string(self):
-  self.MyStr=input("enter any string:,")
-def print_string(self):
- s=self.MyStr
-print("String in upper Case:",s.upper())
 
-M=MyClass()
-M.get_string()
-M.print_string()
+class MyClass:
+
+                def Get_String(self):
+
+                                self.MyStr=input("Enter any String: ")
+
+                def Print_String(self):
+
+                                s=self.MyStr
+
+                                print("String in Upper Case: " , s.upper())
+
+# main body
+
+Obj=MyClass()
+
+Obj.Get_String()
+
+Obj.Print_String()
